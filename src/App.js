2abd483639c97812c694b98bea87e18bf759891b
wj <944064720@qq.com>
2019-11-21 13:18:50 +0800

    #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Square from './square'
+import Header from './header.js'
+// import { Router, Route, Link } from 'react-router'
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route,Link,useRouteMatch} from "react-router-dom";
+
+function My(){
+  // let march = useRouteMatch()
+  // let { My } = ;
+  console.log('路由1',useRouteMatch())
+  return <div>123</div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <div>
+             <ul>
+                <li><Link to="/squa">1</Link></li>
+                <li><Link to="/header">2</Link></li>
+                <li><Link to="/my/1">3</Link></li>
+             </ul>
+          </div>
+          <Switch>
+          <Route path="/squa">
+            <Square />
+          </Route>
+          <Route path="/header">
+            <Header />
+          </Route>
+          <Route path="/my/:id">
+            <My />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class HeaderInfo extends React.Component{
+   constructor(props){
+      super(props)
+      this.setState = {}
+   }
+   render(){
+      return <div >你好啊</div>
+   }
+}
+export default HeaderInfo
